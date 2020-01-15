eecce7280a51bff0e824cc8df92028bf3ac6398d
ijerosimic <igor.jerosimic1@gmail.com>
2020-01-15 14:00:56 +0100

    Add circle, drawing classes

---
+++ Drawing/Drawing.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace Widget
+{
+    public class Drawing
+    {
+        public List<WidgetBase> Widgets { get; set; }
+
+        public Drawing()
+        {
+            Widgets = new List<WidgetBase>();
+        }
+
+        public void Add(WidgetBase widget)
+        {
+            Widgets.Add(widget);
+        }
+
+        public IEnumerable<string> Print()
+        {
+            foreach (var widget in Widgets)
+                yield return widget.Display();
+        }
+    }
+}
---
+++ Drawing/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Widget.Widgets;
+
+namespace Widget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var drawing = new Drawing();
+
+            drawing.Add(new Circle(5, 10));
+
+            foreach (var widget in drawing.Print())
+                Console.WriteLine(widget);
+        }
+    }
+}
