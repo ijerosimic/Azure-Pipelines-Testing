37fb25666158bd6815a709cf94e4fccc0920df0a
ijerosimic <igor.jerosimic1@gmail.com>
2020-01-16 15:28:29 +0100

    Food Controller

---
+++ WebApplication1/Controllers/FoodController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class FoodController : ControllerBase
+    {
+        // GET: Food
+        public ActionResult<string> Get()
+        {
+            return "GET YOUR FOOD HERE!";
+        }
+    }
+}
